1941eacb5a566c8e539107cd6ea58c7f22a481cb
Md Anas Sabah <mdanassabah@gmail.com>
2023-01-06 00:22:14 +0530

    Chapter-2 Igniting our app

---
+++ Chapter-2 Igniting our app/dist/index.6bd02f5a.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1", {
+    id: "title"
+}, "Hello World Using React-H1");
+const heading2 = React.createElement("h1", {
+    id: "title"
+}, "Hello World Using React-H2");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading2
+]);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(container);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
